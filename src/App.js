d4d70c69bbfaddd16d743a3f2b181427e79d9125
SiminDev <simin.rahmani94@gmail.com>
2022-08-07 23:52:41 +0430

    add homepage route to app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import * as React from "react";
+import Home from "./Pages/Home/home";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="App flex flex-row flex-wrap w-screen min-h-screen overflow-hidden	 sm:flex-nowrap md:h-screen ">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

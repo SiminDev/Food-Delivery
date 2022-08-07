37a118f37d6a2545471ca9d15a88ecbef17d5d04
SiminDev <simin.rahmani94@gmail.com>
2022-08-07 23:40:46 +0430

    setup primary component and folder structure

---
+++ src/Components/IconComponents/MinusIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MinusIcon({ className }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.76926 10.9999L15.2308 10.9999"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
+
+export default MinusIcon;
---
+++ src/Components/IconComponents/PlusIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function PlusIcon({ className }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M11 15.2307V6.76915" strokeWidth="1.5" strokeLinecap="round" />
+      <path
+        d="M6.76926 10.9999L15.2308 10.9999"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
+
+export default PlusIcon;

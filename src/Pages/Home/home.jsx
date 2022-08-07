bf70e585961c6d5c829e18a77a6c7b2b68b0bc9d
SiminDev <simin.rahmani94@gmail.com>
2022-08-08 00:37:40 +0430

    import category and products

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -3,6 +3,8 @@ import Navigation from "../../Components/navigation";
 import Content from "../../Components/content";
 import Sidebar from "../../Components/sidebar";
 import Header from "./header";
+import Category from "./category";
+import Products from "./products";
 
 function Home() {
   return (
@@ -10,6 +12,8 @@ function Home() {
       <Navigation />
       <Content>
         <Header />
+        <Category />
+        <Products />
       </Content>
       <Sidebar></Sidebar>
     </>

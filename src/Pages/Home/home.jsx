8762c088ad08c94b4dacc1c415940d1a0416f0ab
SiminDev <simin.rahmani94@gmail.com>
2022-08-08 00:32:20 +0430

    import header to homepage

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navigation from "../../Components/navigation";
 import Content from "../../Components/content";
 import Sidebar from "../../Components/sidebar";
+import Header from "./header";
 
 function Home() {
   return (
     <>
       <Navigation />
-      <Content></Content>
+      <Content>
+        <Header />
+      </Content>
       <Sidebar></Sidebar>
     </>
   );

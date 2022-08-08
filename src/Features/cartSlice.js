33eb9e2ba21e518582d31fac13ea3aee0dd01c56
SiminDev <simin.rahmani94@gmail.com>
2022-08-08 12:50:25 +0430

    initial config of cartSlice

---
+++ src/Features/cartSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+});
+
+export const {} = cartSlice.actions;
+export default cartSlice.reducer;

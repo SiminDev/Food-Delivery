8d23fdd689c0bd612ef51e1a884b5ed08f1a7085
SiminDev <simin.rahmani94@gmail.com>
2022-08-08 11:33:07 +0430

    import orderList component

--- src/Pages/Invoice/invoice.jsx
+++ src/Pages/Invoice/invoice.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navigation from "../../Components/navigation";
 import Content from "../../Components/content";
 import Sidebar from "../../Components/sidebar";
+import OrderList from "./orderList";
 
 function Invoice() {
   return (
     <>
       <Navigation />
-      <Content></Content>
+      <Content>
+        <OrderList />
+      </Content>
       <Sidebar></Sidebar>
     </>
   );

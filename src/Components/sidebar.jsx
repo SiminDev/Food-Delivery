e513fc6129cf9db1c8ba3d26b44d0f8d5e6cad4e
SiminDev <simin.rahmani94@gmail.com>
2022-08-08 23:12:09 +0430

    add cart total quantity from redux

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import CartIcon from "./IconComponents/CartIcon";
+import { useSelector } from "react-redux";
 
 function Sidebar(props) {
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
+
   return (
     <div className="w-full bg-white md:w-1/4 p-10 font-body">
       <div className="flex justify-end mb-5">
         <button className="w-9 h-9 rounded-full bg-secondary-100 flex justify-center items-center relative">
           <CartIcon className={"w-6 h-6"} />
           <span className="w-4 h-4 absolute top-1 right-0 flex justify-center items-center rounded-full bg-primary text-white font-number text-xxs">
-            {3}
+            {cartTotalQuantity}
           </span>
         </button>
       </div>

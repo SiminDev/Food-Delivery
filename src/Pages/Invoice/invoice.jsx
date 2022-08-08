24b59af5b01ff62e6353f754db3da575b68caf8d
SiminDev <simin.rahmani94@gmail.com>
2022-08-08 11:24:59 +0430

    create invoice page component

---
+++ src/Pages/Invoice/invoice.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "../../Components/navigation";
+import Content from "../../Components/content";
+import Sidebar from "../../Components/sidebar";
+
+function Invoice() {
+  return (
+    <>
+      <Navigation />
+      <Content></Content>
+      <Sidebar></Sidebar>
+    </>
+  );
+}
+
+export default Invoice;
